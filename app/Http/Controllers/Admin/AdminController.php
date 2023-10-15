5e8fbe25fdf539d409c04c3c7e9e4b8f64884111
wehmamxx <readytosurff@gmail.com>
2023-10-15 15:15:04 +0700

    update

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function dashboard() {
-        return view("admin.pages.dashboard");
+        $books = Book::count();
+        $authors = Author::count();
+        $users = User::count();
+        return view("admin.pages.dashboard", compact("books", "authors", "users"));
     }
 }
--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -10,7 +10,7 @@
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                         Total Books</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalEvent ?? 0 }}</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $books ?? 0 }}</div>
                 </div>
                 <div class="col-auto">
                     <i class="fas fa-list-alt fa-2x text-gray-300"></i>
@@ -27,7 +27,7 @@
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                         Total Authors</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalAdmin ?? 0 }}</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $authors ?? 0 }}</div>
                 </div>
                 <div class="col-auto">
                     <i class="fas fa-shopping-bag  fa-2x text-gray-300"></i>
@@ -43,8 +43,8 @@
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                        Total Admin</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalRegister ?? 0 }}</div>
+                        Total Admin/User</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $users ?? 0 }}</div>
                 </div>
                 <div class="col-auto">
                     <i class="fas fa-shopping-basket fa-2x text-gray-300"></i>
